454558cf4d10ac046d389d580d42a01c21aea097
tnfchlsaud-boop <tnfchlsaud@gmail.com>
2026-09-02 21:03:10 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='hgp-report-v3';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
